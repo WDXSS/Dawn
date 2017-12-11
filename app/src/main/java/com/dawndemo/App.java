d7896b06a22aed8db643ffabae3b209b5581bab2
zhouchao <810753835@qq.com>
2017-12-11 10:13:13 +0800

    全部提交到git

--- app/src/main/java/com/dawndemo/App.java
+++ app/src/main/java/com/dawndemo/App.java
@@ -1,14 +1,17 @@
 package com.dawndemo;
 
 import android.app.Application;
+import android.util.Log;
 
 /**
  * Created by zc on 2017/3/30.
  */
 
 public class App extends Application {
+    private static final String TAG = "App";
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i(TAG, "onCreate: company_zc");
     }
 }
