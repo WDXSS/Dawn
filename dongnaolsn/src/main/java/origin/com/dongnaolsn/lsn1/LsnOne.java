47a56868cc68ca68bc00211fad3e73305a6570dc
zhouchao <810753835@qq.com>
2018-06-12 18:25:32 +0800

    课程补充

--- dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn1/LsnOne.java
+++ dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn1/LsnOne.java
@@ -1,20 +1,19 @@
-package lsn1;
+package origin.com.dongnaolsn.lsn1;
 
-import android.app.ActivityManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
-import com.dawndemo.base.BaseActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by zc on 2018/6/11.
  */
 
-public class LsnOne extends BaseActivity{
+public class LsnOne extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 //        setContentView();
+        
     }
 }
