27029156099a282ed5ba70a0275d5fd5f786ddaa
zhouc <810753835@qq.com>
2018-06-13 23:23:29 +0800

    Span总结

---
+++ dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn2/LsnTwo.java
@@ -0,0 +1,16 @@
+package origin.com.dongnaolsn.lsn2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by zc on 2018/6/13.
+ */
+
+public class LsnTwo extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
