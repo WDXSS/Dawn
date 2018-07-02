7d56c6aca0da26f95512dba7beabfdc739fe8312
zhouc <810753835@qq.com>
2018-07-02 22:38:26 +0800

    Span总结

--- dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn1/LsnOne.java
+++ dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn1/LsnOne.java
@@ -13,7 +13,7 @@ public class LsnOne extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        setContentView();
+//       setContentView();
         
     }
 }
--- dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn2/LsnTwo.java
+++ dongnaolsn/src/main/java/origin/com/dongnaolsn/lsn2/LsnTwo.java
@@ -3,8 +3,14 @@ package origin.com.dongnaolsn.lsn2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 /**
+ *绘制：
+ *  ViewRootImpl.performTraversals() 调用测量，摆放,-绘制
+ *  1.测量 performMeasure();
+ *  2.
+ * View.MeasureSpec 作用View在Measure过程中根据<br/>
  * Created by zc on 2018/6/13.
  */
 
@@ -12,5 +18,7 @@ public class LsnTwo extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView();
+
     }
 }
