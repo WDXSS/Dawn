cf679bb90dea77d06c099697eae232fd0de268ef
zhouchao <810753835@qq.com>
2018-07-04 19:03:23 +0800

    课程补充

---
+++ dongnaolsn/src/main/java/origin/com/dongnaolsn/txt/lsn10.java
@@ -0,0 +1,26 @@
+package origin.com.dongnaolsn.txt;
+
+import android.view.MotionEvent;
+
+/**
+ * lsn 10 学习
+ * 1.source  怎么导入
+ * Created by zc on 2018/7/4.
+ */
+
+public class lsn10 {
+
+    /**
+     * {@link android.app.Activity#onUserInteraction()} 介绍<br/>
+     */
+    public void onUserInteraction() {
+    }
+
+    /**
+     * {@link android.view.Window#superDispatchTouchEvent(MotionEvent)} 介绍<br/>
+     *{@link com.android.internal.policy.PhoneWindow#superDispatchTouchEvent(MotionEvent)} 介绍<br/>
+     */
+   public void superDispatchTouchEvent(){
+
+    }
+}
