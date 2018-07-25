b58f81fb88e29091169bc553d18fd67d473790b3
zhouchao <810753835@qq.com>
2018-07-25 19:30:03 +0800

    课程补充

--- app/src/main/java/com/dawndemo/ui/anim/CollapsingToolbarActivity.java
+++ app/src/main/java/com/dawndemo/ui/anim/CollapsingToolbarActivity.java
@@ -5,6 +5,7 @@ import com.dawndemo.base.BaseActivity;
 /**
  * 折叠式标题栏
  * CollapsingToolbarLayout
+ * https://www.jianshu.com/p/564a0c56022b
  */
 public class CollapsingToolbarActivity extends BaseActivity{
 }
