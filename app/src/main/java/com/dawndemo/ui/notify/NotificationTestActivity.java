a877102d91b8aabdf63d2883330e91df4561f9cf
zhouchao <810753835@qq.com>
2018-08-28 21:46:44 +0800

    通知栏8.0

--- app/src/main/java/com/dawndemo/ui/notify/NotificationTestActivity.java
+++ app/src/main/java/com/dawndemo/ui/notify/NotificationTestActivity.java
@@ -1,36 +1,43 @@
 package com.dawndemo.ui.notify;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 
+import com.dawndemo.R;
 import com.dawndemo.base.BaseActivity;
 
 /**
  * 8.0的通知 26以上<br>
  * <a href="https://www.jb51.net/article/138411.htm"> 8.0的通知介绍</a> <br>
+ *     https://blog.csdn.net/guolin_blog/article/details/79854070 <br>
  * Created by Chao on 2018/8/13
  */
 public class NotificationTestActivity extends BaseActivity {
-
+    private String channelChat = "chat";
+    private String channelSubscribe = "subscribe";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_notify_test);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             //创建两个渠道
-            String channelId = "chat";
             String channelName = "聊天消息";
             int importance = NotificationManager.IMPORTANCE_HIGH;//重要等级
-            createNotificationChannel(channelId, channelName, importance);
+            createNotificationChannel(channelChat, channelName, importance);
 
-            channelId = "subscribe";
             channelName = "订阅消息";
             importance = NotificationManager.IMPORTANCE_DEFAULT;//重要等级
-            createNotificationChannel(channelId, channelName, importance);
+            createNotificationChannel(channelSubscribe, channelName, importance);
         }
     }
 
@@ -50,4 +57,17 @@ public class NotificationTestActivity extends BaseActivity {
     }
 
 
+    public void doNotify(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, channelChat)
+                .setContentTitle("收到一条聊天消息")
+                .setContentText("今天中午吃什么？")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setAutoCancel(true)
+                .build();
+        assert manager != null;
+        manager.notify(1, notification);
+    }
 }
